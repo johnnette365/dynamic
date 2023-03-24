ca8ec25fac889eceb9543e799ac3eec481b4e495
johnnette365 <103301325+johnnette365@users.noreply.github.com>
2023-03-24 12:00:54 +0530

    Delete johnnette_app directory

--- johnnette_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from johnnette_app.models import Products
-
-# Register your models here.
-
-admin.site.register(Products)
--- johnnette_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class JohnnetteAppConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "johnnette_app"
